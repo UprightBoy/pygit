4d07f79c556e5dec0180dfa4ac6834e9df7a6e37
gengzhi <1178854439@qq.com>
2017-04-16 17:01:13 +0800

    解析html文件的正文部分..\S\S,BeautifulSoup都是什么？

---
+++ www/static/littleProject/htmlAnalysis/HTMLAnalysis.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import re
+from html.parser import HTMLParser
+
+'''
+def html_body(file):
+    with open(file, encoding='UTF-8') as f:
+        re_body = re.compile(r'<li>.*</li>')
+        if re_body.match(f.read()):
+            print('ok')
+            return re_body.match(f.read())
+
+if __name__ == '__main__':
+    print(html_body('D:\pygit\www\static\littleProject\htmlAnalysis/testHTML.html'))
+
+
+
+class MyHTMLParser(HTMLParser):
+
+    def handle_starttag(self, tag, attrs):
+        print('<%s>', tag)
+
+    def handle_endtag(self, tag):
+        print('</%s>' % tag)
+
+    def handle_startendtag(self, tag, attrs):
+        print('<%s/>' % tag)
+
+    def handle_data(self, data):
+        print(data)
+
+    def handle_entityref(self, name):
+        print('&%s' % name)
+
+    def handle_comment(self, data):
+        print('<!--', data, '-->')
+
+    def handle_charref(self, name):
+        print('&#%s' % name)
+
+parser = MyHTMLParser()
+
+
+def html_body(file):
+    with open(file, encoding='UTF-8') as f:
+        parser.feed(f.read())
+
+if __name__ == '__main__':
+    print(html_body('D:\pygit\www\static\littleProject\htmlAnalysis/testHTML.html'))
+'''
+
+import requests, re
+from bs4 import BeautifulSoup
+
+url = 'http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958' \
+      'f449949df42a6d3a2e542c000/001432688314740a0aed473a39f47b09c8c7274c9ab6aee000'
+data = requests.get(url)
+r = re.findall(r'<body>[\s\S]*</body>', data.text)
+print(r[0])
+
+print('-----------------------------------------------------------------------------------')
+soup = BeautifulSoup(data.text, 'html.parser')
+print(soup.body.text)
+
+
