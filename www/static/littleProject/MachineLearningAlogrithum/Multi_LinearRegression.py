7905ac5bcb052e0654dbb70521c978e5b217f547
gengzhi <1178854439@qq.com>
2017-05-17 22:02:15 +0800

    加入机器学习

--- www/static/littleProject/MachineLearningAlogrithum/Multi_LinearRegression.py
+++ www/static/littleProject/MachineLearningAlogrithum/Multi_LinearRegression.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv('Advertising.csv')
+print(data.head())
+print(data.tail())
+print(data.shape)
